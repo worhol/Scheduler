d9b9d578759c8926f4448d9a6cbaeed7a00bbea8
worhol <trashuries@gmail.com>
2022-09-19 20:09:32 -0500

    Login UI work in progress

--- src/main/java/wgu/softwaretwo/samircokic/SchedulerApplication.java
+++ src/main/java/wgu/softwaretwo/samircokic/SchedulerApplication.java
@@ -2,17 +2,23 @@ package wgu.softwaretwo.samircokic;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.ImagePattern;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class SchedulerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SchedulerApplication.class.getResource("LoginForm.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
